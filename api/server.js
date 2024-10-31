669475f7795bad5024bdc3fd585580bdf13e6cb0
Robert Jank <robert.jank@yahoo.com>
2024-10-31 13:38:18 -0500

    WIP

--- api/server.js
+++ api/server.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const { logger } = require("./middleware/middleware");
 const router = require("./router/router");
+const cors = require("cors");
 
 const server = express();
+
+server.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
 server.use(express.json({ limit: "10mb" }));
 server.use(express.urlencoded({ limit: "10mb", extended: true }));
