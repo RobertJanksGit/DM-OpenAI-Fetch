a2a69418f5797b7b62377ebe03f8edcf13d715d0
Robert Jank <robert.jank@yahoo.com>
2024-11-04 21:30:40 -0600

    wip

--- api/server.js
+++ api/server.js
@@ -2,22 +2,16 @@ const express = require("express");
 const { logger } = require("./middleware/middleware");
 const router = require("./router/router");
 const cors = require("cors");
-const port = require("../index")
 
 const server = express();
 
-port === process.env.PORT ? server.use(
+server.use(
   cors({
     origin: "https://dungeon-master-pro.firebaseapp.com",
     methods: ["GET", "POST"],
     credentials: true,
   })
-) : cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
-    credentials: true,
-  })
-)
+);
 
 server.use(express.json({ limit: "10mb" }));
 server.use(express.urlencoded({ limit: "10mb", extended: true }));
